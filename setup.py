438bb089e24d22ad06a65d2324df85af0610563c
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-15 21:18:54 +0500

    change package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi_authtools',
-    version='0.2',
+    version='0.3',
     url='https://github.com/michael7nightinglae/fastapi_authtools',
     install_requires=[
         "fastapi", "python-jose"
