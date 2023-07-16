eac1644fa4330e86b39629e34e6f0cfcabdf438f
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-17 00:28:39 +0500

    refactor raise and messages

--- fastapi_authtools/exceptions.py
+++ fastapi_authtools/exceptions.py
@@ -1,22 +1,45 @@
-from fastapi.exceptions import HTTPException
+from fastapi import HTTPException
 
 
-def raise_credentials_error():
+def credentials_error_message() -> str:
+    return "Auth credentials are not provided."
+
+
+def permissions_error_message() -> str:
+    return "You don`t have permissions."
+
+
+def data_model_exception_message(expected, got) -> str:
+    return f"""Invalid data model got: {got}. Expected: {expected}."""
+
+
+def invalid_credentials_message() -> str:
+    return "Invalid credentials provided."
+
+
+def raise_credentials_error() -> str:
     raise HTTPException(
         status_code=401,
-        detail="Auth credentials are not provided."
+        detail=credentials_error_message()
     )
 
 
-def raise_permissions_error():
+def raise_permissions_error() -> str:
     raise HTTPException(
         status_code=403,
-        detail="You don`t have permissions."
+        detail=permissions_error_message()
     )
 
 
-def raise_data_model_exception(expected, got):
+def raise_data_model_exception(expected, got) -> str:
     raise HTTPException(
         status_code=400,
-        detail=f"""Invalid data model got: {got}. Expected: {expected}."""
+        detail=data_model_exception_message(expected, got)
+    )
+
+
+def raise_invalid_credentials() -> str:
+    raise HTTPException(
+        status_code=400,
+        detail=invalid_credentials_message()
     )
