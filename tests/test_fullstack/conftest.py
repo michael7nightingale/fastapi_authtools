a6bf262d22d0519600831c88aebc36ee4175c357
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-08 14:46:24 +0500

    create fullstack example tests

---
+++ tests/test_fullstack/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from httpx import AsyncClient
+
+from .app import app as application
+
+
+@pytest.fixture
+def admin() -> dict:
+    return {
+        "username": "admin",
+        "password": "password_for_admin"
+    }
+
+
+@pytest.fixture
+def michael7nightingale() -> dict:
+    return {
+        "username": "michael7nightingale",
+        "password": "password"
+    }
+
+
+@pytest.fixture
+async def app():
+    yield application
+
+
+@pytest.fixture
+async def client(app):
+    async with AsyncClient(app=app, base_url="http://localhost:8008") as client_:
+        yield client_
+
+
+@pytest.fixture
+async def authorized_client(client: AsyncClient, michael7nightingale: dict):
+    resp = await client.post(url_path_for("login_post"), data=michael7nightingale)
+    client.cookies['access-token'] = resp.cookies['access-token']
+    yield client
+
+
+url_path_for = lambda name, **kwargs: application.url_path_for(name, **kwargs)
---
+++ tests/test_fullstack/test_auth.py
@@ -0,0 +1,51 @@
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+
+from .conftest import url_path_for
+
+
+class TestLogin:
+
+    async def test_login_success(self, client: AsyncClient, michael7nightingale: dict):
+        resp = await client.post(url_path_for("login_post"), data=michael7nightingale)
+        assert resp.status_code == status.HTTP_303_SEE_OTHER
+        assert "access-token" in resp.cookies
+
+    async def test_login_fail(self, client: AsyncClient):
+        user_data = {
+            "username": "string",
+            "password": "string_password"
+        }
+        resp = await client.post(url_path_for("login_post"), data=user_data)
+        assert resp.status_code == status.HTTP_400_BAD_REQUEST
+        assert "access-token" not in resp.json()
+        assert resp.json() == {"detail": "Cannot find user with this credentials."}
+
+    async def test_login_fail_password_invalid(self, client: AsyncClient, admin):
+        user_data = {
+            "username": admin['username'],
+            "password": "string_password_+asdas"
+        }
+        resp = await client.post(url_path_for("login_post"), data=user_data)
+        assert resp.status_code == status.HTTP_400_BAD_REQUEST
+        assert "access-token" not in resp.json()
+        assert resp.json() == {"detail": "Password is invalid."}
+
+
+class TestMeLoginRequired:
+
+    async def test_me_success(self, authorized_client: AsyncClient):
+        resp = await authorized_client.get(url_path_for("me"))
+        assert resp.status_code == status.HTTP_200_OK
+
+    async def test_me_fail_tokenless(self, client: AsyncClient):
+        resp = await client.get(url_path_for("me"))
+        assert resp.status_code == status.HTTP_401_UNAUTHORIZED
+        assert resp.json() == {"detail": 'Auth credentials are not provided.'}
+
+    async def test_me_fail_token_invalid(self, client: AsyncClient):
+        client.headers['Authorization'] = "Token 124-i0lfkajnlfksajkfs;ai 0f-aojnf"
+        resp = await client.get(url_path_for("me"))
+        assert resp.status_code == status.HTTP_401_UNAUTHORIZED
+        assert resp.json() == {"detail": 'Auth credentials are not provided.'}
