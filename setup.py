db1ab0458851d08fc639ad382b004035a24d8619
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-08 14:44:44 +0500

    update python project configuration (repository url, verison)

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi_authtools',
-    version='0.3',
-    url='https://github.com/michael7nightinglae/fastapi_authtools',
+    version='0.5',
+    url='https://github.com/michael7nightingale/fastapi_authtools',
     install_requires=[
-        "fastapi", "python-jose"
+        "fastapi==0.100.0", "python-jose",
+        'pydantic==2.0.2'
     ],
     license='MIT',
     author='Michael Nightingale',
     author_email='suslanchikmopl@gmail.com',
     description='Simple and comfortable FastAPI JWT authentication.',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests', "app.py"]),
+    long_description_content_type="Markdown",
     long_description=open('README.md').read(),
     zip_safe=False
 )
